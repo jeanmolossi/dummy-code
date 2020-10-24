bcda30f2012fd244fa99f174bff54550ea45af60
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-10-24 17:44:41 -0300

    Refact: Lessons now is Modules

--- src/pages/Module/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { MainLayout } from '../../components';
-import { Container } from './styles';
-
-const Module: React.FC = () => {
-  return <MainLayout />;
-};
-
-export default Module;
