5bc89a7039b921c38d86bc9818f5b6d6f13bf244
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-10-24 16:41:44 -0300

    New: Profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,88 @@
 import React from 'react';
+import { FaTwitch } from 'react-icons/fa';
+import {
+  FiAward,
+  FiGithub,
+  FiInstagram,
+  FiLinkedin,
+  FiSend,
+  FiYoutube,
+} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import { MainLayout } from '../../components';
-import { Container } from './styles';
+import {
+  HeroUser,
+  Avatar,
+  UserActivity,
+  UserSocial,
+  Socials,
+  Points,
+  UserBio,
+} from './styles';
 
 const Profile: React.FC = () => {
   return (
     <MainLayout>
-      <Container />
+      <HeroUser>
+        <Avatar>
+          <img
+            src="http://192.168.0.104:3000/files/image2.jpg"
+            alt="User Avatar"
+          />
+          <h1>Nome</h1>
+        </Avatar>
+      </HeroUser>
+
+      <UserActivity>
+        <div>
+          1.3k
+          <small>Atividades</small>
+        </div>
+
+        <div>
+          835
+          <small>Favoritos</small>
+        </div>
+
+        <div>
+          37.4k
+          <small>Curtidas</small>
+        </div>
+      </UserActivity>
+
+      <UserSocial>
+        <Socials>
+          <Link to="/" className="instagram">
+            <FiInstagram />
+          </Link>
+          <Link to="/" className="linkedin">
+            <FiLinkedin />
+          </Link>
+          <Link to="/" className="telegram">
+            <FiSend />
+          </Link>
+          <Link to="/" className="youtube">
+            <FiYoutube />
+          </Link>
+          <Link to="/" className="github">
+            <FiGithub />
+          </Link>
+          <Link to="/" className="twitch">
+            <FaTwitch />
+          </Link>
+        </Socials>
+        <Points>
+          <FiAward />
+          <small>12 pontos</small>
+        </Points>
+      </UserSocial>
+
+      <UserBio>
+        Estamos enfrentando um sério dilema de negócios, com o Facebook tirando
+        uma boa parte do tráfego para sites de notícias e conteúdo, e os
+        bloqueadores de anúncios consumindo o que sobrou dele enquanto cortam as
+        receitas de publicidade.
+      </UserBio>
     </MainLayout>
   );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,171 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const HeroUser = styled.div`
+  height: 29rem;
+  overflow-y: hidden;
+  background-image: url('http://localhost:3000/files/image1.jpg');
+  display: flex;
+  place-items: center;
+  place-content: center;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: linear-gradient(#2a3c4400, #30444e);
+    z-index: 1;
+  }
+`;
+
+export const Avatar = styled.div`
+  display: block;
+  width: var(--size-xl7);
+  height: var(--size-xl7);
+
+  > h1 {
+    position: relative;
+    z-index: 3;
+    text-align: center;
+    color: #fff;
+    margin-top: var(--spacing-md);
+  }
+
+  > img {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: var(--radii-full);
+    border: 0.4rem solid var(--green-100);
+    z-index: 3;
+  }
+`;
+
+export const UserActivity = styled.div`
+  background-color: var(--grey-300);
+  display: flex;
+  justify-content: space-evenly;
+  padding: var(--padding-md) 0;
+
+  > div {
+    text-align: center;
+    font-size: var(--size-lg);
+    font-weight: bold;
+    color: var(--grey-100);
+
+    > small {
+      display: block;
+      font-size: var(--size-md);
+    }
+  }
+`;
+
+export const UserSocial = styled.div`
+  background-color: var(--grey-300);
+  display: flex;
+  position: relative;
+  border-bottom-left-radius: var(--radii-xxl);
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: var(--radii-xl1);
+    height: var(--radii-xl1);
+    background-color: var(--grey-300);
+    top: 100%;
+    right: 0;
+    z-index: -1;
+  }
+`;
+
+export const Socials = styled.div`
+  display: flex;
+  gap: var(--spacing-md);
+  padding: var(--padding-md);
+  flex: 1;
+  overflow-x: auto;
+
+  > div,
+  a {
+    padding: var(--padding-sm);
+    border-radius: var(--radii-full);
+    display: flex;
+    place-content: center;
+    place-items: center;
+    font-size: var(--size-lg);
+    color: #fff;
+  }
+
+  .instagram {
+    background-image: radial-gradient(
+      132% 92% at 33% 91%,
+      #feda77 0%,
+      #f58529 25%,
+      #dd2a7b 50%,
+      #8134af 75%,
+      #515bd4 100%
+    );
+  }
+
+  .linkedin {
+    background-color: #0077b5;
+  }
+  .telegram {
+    background-color: #0088cc;
+  }
+  .youtube {
+    background-color: #c4302b;
+  }
+  .github {
+    background-color: #000;
+  }
+  .twitch {
+    background-color: #772be8;
+  }
+`;
+
+export const Points = styled.div`
+  width: 9.6rem;
+
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  place-content: center;
+  font-size: var(--size-md);
+
+  > svg {
+    font-size: var(--size-lg);
+  }
+
+  > small {
+    color: var(--grey-100);
+  }
+`;
+
+export const UserBio = styled.div`
+  padding: var(--padding-lg);
+  background-image: var(--dark-100);
+  flex: 1;
+  border-top-right-radius: var(--radii-xxl);
+  position: relative;
+  z-index: -1;
+  line-height: var(--spacing-lg);
+  font-size: var(--size-md);
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: var(--radii-xxl);
+    height: var(--radii-xxl);
+    background-color: var(--dark-200);
+    bottom: 100%;
+    left: 0;
+    z-index: -1;
+  }
+`;
