83224ac190fa76c3f385991526401a243d348793
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-11-21 03:00:39 -0300

    new: confirm account page exported

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 export { default as SignIn } from './SingIn';
 export { default as SignUp } from './SignUp';
 export { default as ForgotPassword } from './ForgotPassword';
+export { default as ConfirmAccount } from './ConfirmAccount';
 export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as Modules } from './Modules';
