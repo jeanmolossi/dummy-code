30fe2630c8d65a24810dfa65c2b7c3dffcaaead1
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-11-21 03:00:26 -0300

    new: auth function

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,20 +1,56 @@
 import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
-import { FiArrowRight, FiLock, FiUser } from 'react-icons/fi';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { FiArrowRight, FiLock, FiMail, FiUser } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import { MainLayout, Input, Button } from '../../components';
+import {
+  RequestStatusEnum,
+  UpdateRequestStatus,
+} from '../../store/modules/app/actions';
 import { Container, Logo, ButtonsContainer } from './styles';
 
 const SignIn = () => {
+  const dispatch = useDispatch();
   const history = useHistory();
 
   const handleSubmit = useCallback(
-    data => {
-      // eslint-disable-next-line no-console
-      console.log(data);
-      history.push('/home');
+    async data => {
+      const { email, password } = data;
+
+      dispatch(
+        UpdateRequestStatus('PENDING', 'Acessando sua conta, só um minutinho'),
+      );
+
+      const { status, user } = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(userAuth => {
+          const { user: ensureAuthUser } = userAuth;
+          return {
+            status: RequestStatusEnum.RESOLVE,
+            user: ensureAuthUser,
+          };
+        })
+        .catch(() => ({ status: RequestStatusEnum.REJECT, user: null }));
+
+      const message =
+        status === 'REJECT'
+          ? 'Não foi possível fazer o login, desculpe'
+          : 'Acesso liberado! Faça bom proveito =)';
+
+      dispatch(UpdateRequestStatus(status, message));
+
+      if (user) {
+        return user.emailVerified
+          ? history.push('/home')
+          : history.push('/confirm-account');
+      }
+      return null;
     },
-    [history],
+    [dispatch, history],
   );
 
   return (
@@ -26,13 +62,13 @@ const SignIn = () => {
         <h2>Acesse para continuar</h2>
 
         <Form onSubmit={handleSubmit}>
-          <Input icon={FiUser} name="username" placeholder="Nick name" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
 
           <Input
             icon={FiLock}
             name="password"
             type="password"
-            placeholder="Sue senha ultra-secreta"
+            placeholder="Sua senha ultra-secreta"
           />
 
           <ButtonsContainer>
