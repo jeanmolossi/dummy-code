4ccc4de1cb9b486289fdbf366fba6c29bfe4220f
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-11-21 22:50:37 -0300

    refactor: changed to context signIn

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,57 +1,13 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Form } from '@unform/web';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { FiArrowRight, FiLock, FiMail, FiUser } from 'react-icons/fi';
-import { useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
+import { FiArrowRight, FiLock, FiMail } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import { MainLayout, Input, Button } from '../../components';
-import {
-  RequestStatusEnum,
-  UpdateRequestStatus,
-} from '../../store/modules/app/actions';
+import { useAuth } from '../../contexts/Auth';
 import { Container, Logo, ButtonsContainer } from './styles';
 
 const SignIn = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const handleSubmit = useCallback(
-    async data => {
-      const { email, password } = data;
-
-      dispatch(
-        UpdateRequestStatus('PENDING', 'Acessando sua conta, só um minutinho'),
-      );
-
-      const { status, user } = await firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(userAuth => {
-          const { user: ensureAuthUser } = userAuth;
-          return {
-            status: RequestStatusEnum.RESOLVE,
-            user: ensureAuthUser,
-          };
-        })
-        .catch(() => ({ status: RequestStatusEnum.REJECT, user: null }));
-
-      const message =
-        status === 'REJECT'
-          ? 'Não foi possível fazer o login, desculpe'
-          : 'Acesso liberado! Faça bom proveito =)';
-
-      dispatch(UpdateRequestStatus(status, message));
-
-      if (user) {
-        return user.emailVerified
-          ? history.push('/home')
-          : history.push('/confirm-account');
-      }
-      return null;
-    },
-    [dispatch, history],
-  );
+  const { signIn } = useAuth();
 
   return (
     <MainLayout showBottomTabs={false}>
@@ -61,7 +17,7 @@ const SignIn = () => {
         <h1>Wellcome!</h1>
         <h2>Acesse para continuar</h2>
 
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={signIn}>
           <Input icon={FiMail} name="email" placeholder="E-mail" />
 
           <Input
