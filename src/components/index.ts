ef7e0f03da91051b9f51eb2eb3e3396c1b45b8c1
Vicente <vicente19981@live.com>
2020-11-22 01:51:00 -0300

    feat: create component text area

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Button } from './shared/Button';
 export { default as SuccessModal } from './layout/SuccessModal';
 export { default as UnsuccessModal } from './layout/UnsuccessModal';
 export { default as BottomTabs } from './layout/BottomTabs';
+export { default as TextArea } from './shared/TextArea';
